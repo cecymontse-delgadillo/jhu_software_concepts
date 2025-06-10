9cb9d3eca310f06b5bff39757963fc8e3c778471
cecymontse-delgadillo <montse.r2d2@gmail.com>
2025-06-09 22:53:14 -0700

    Backfone of module_4, pizza.py and order.py created

---
+++ module_4/order.py
@@ -0,0 +1,20 @@
+class Order:
+    def __init__(self):
+        #Initializes a customer order
+        #initialize order cost
+        return 1
+    
+    def __str__(self):
+        #Print a customers complete order
+        return 1
+
+    def input_pizza(self,crust,sauce,cheese,toppings):
+        #input the customers order for a given pizza
+        #Initialize the pizza object and attach to the order
+        #Update cost
+        return 1
+    
+    def order_paid(self):
+        #Set order as paid once payment has been collected
+        return 1
+        
---
+++ module_4/pizza.py
@@ -0,0 +1,13 @@
+class Pizza:
+    #Pizza objects and associated cost
+    def __init__(self,crust,sauce,cheese,toppings):
+        #Initiatizes Pizza
+        #Set Pizza variables
+        #Set cost to create
+    
+    def __str__(self):
+        #Print a pizza
+        #Print the cost of that pizza
+    
+    def cost(self):
+        #Determine the cost of a pizza
