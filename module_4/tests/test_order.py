8ebf692807b5fbed25d59b02339789fff57a934c
cecymontse-delgadillo <montse.r2d2@gmail.com>
2025-06-12 22:11:51 -0700

    pytest.ini added, initial test_order.py and order.py logic added

--- module_4/tests/test_order.py
+++ module_4/tests/test_order.py
@@ -18,7 +18,7 @@ def example_complex_order():
     order_1.input_pizza("Thick", ["Pesto", "Liv_Sauce", "Marinara"], "Mozzarella", ["Mushrooms", "Pepperoni", "Pineapple"])
     return order_1
 
-
+@pytest.mark.unit
 @pytest.mark.parametrize("attr, expected_result", [
     ("pizzas",[]),
     ("total_cost",0),
@@ -27,7 +27,7 @@ def example_complex_order():
 def test_order_init_function(example_init_order, attr, expected_result):
     assert getattr(example_init_order, attr) == expected_result
 
-
+@pytest.mark.unit
 @pytest.mark.parametrize("fixture_funct, expected_result", [
     ("example_simple_order","Customer Requested:\nCrust: Thin, Sauce: ['Pesto', 'Liv_Sauce'], Cheese: Mozzarella, Toppings: ['Mushrooms', 'Pepperoni'], Cost: 18"),
     ("example_complex_order","Customer Requested:\nCrust: Thin, Sauce: ['Pesto', 'Liv_Sauce'], Cheese: Mozzarella, Toppings: ['Mushrooms', 'Pepperoni'], Cost: 18 \nCrust: Thick, Sauce: ['Pesto', 'Liv_Sauce', 'Marinara'], Cheese: Mozzarella, Toppings: ['Mushrooms', 'Pepperoni', 'Pineapple'], Cost: 22"),
@@ -37,15 +37,19 @@ def test_order_str_function(request, fixture_funct,expected_result):
     order = request.getfixturevalue(fixture_funct)
     assert order.__str__() == expected_result
 
+@pytest.mark.unit
 @pytest.mark.parametrize("fixture_funct, expected_result", [
     ("example_init_order",0),
     ("example_complex_order", 40),
 ])
+
+@pytest.mark.unit
 def test_input_pizza_cost(request, fixture_funct,expected_result):
     #Test method should update cost
     order = request.getfixturevalue(fixture_funct)
     assert order.total_cost == expected_result
 
+@pytest.mark.unit
 def test_order_paid_function(example_init_order):
     #Test method should update paid to true
     assert example_init_order.paid is False
