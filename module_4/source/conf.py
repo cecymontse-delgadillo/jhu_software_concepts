3e3e83bd87197b7491720e1f982064cd08a2d80d
cecymontse-delgadillo <montse.r2d2@gmail.com>
2025-06-14 03:31:35 -0700

    Changes to conf.py

--- module_4/source/conf.py
+++ module_4/source/conf.py
@@ -13,7 +13,7 @@ release = '6/15/2025'
 
 import os
 import sys
-import sphinx_rtd_theme
+import sphinx_rtd_theme 
 sys.path.insert(0,os.path.abspath(".."))
 
 # -- General configuration ---------------------------------------------------
@@ -30,4 +30,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+
